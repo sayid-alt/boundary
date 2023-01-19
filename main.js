6dcef6ff42aed5a6aedafadf7e741b79afb16eee
heykal-crs <heykalcrs@gmail.com>
2023-01-19 17:08:14 +0700

    final cmmit

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ function calc() {
     let startValue = parseInt(document.querySelector('#startValue').value);
     let endValue = parseInt(document.querySelector('#endValue').value);
     let hValue = parseFloat(document.querySelector('#hValue').value);
-    let calculated, result;
+    let calculated, result = [];
 
-    console.log(startValue, endValue, hValue);
+    console.log('sv: ', startValue, ', ev: ', endValue, ', hv:', hValue);
 
     let operator1 = document.querySelector('#operator1').value;
     let operator2 = document.querySelector('#operator2').value;
-    console.log(operator1, operator2)
+    // console.log(operator1, operator2)
 
     const fx = (x) => {
         
@@ -36,7 +36,7 @@ function calc() {
         return calculated;
     };
 
-    console.log( "new",fx(1))
+    // console.log( "new",fx(1))
 
     const fx_integral = (x, h) => {
         calculated = (fx(x + h) - fx(x - h)) / (2 * h);
@@ -50,14 +50,13 @@ function calc() {
 
 
     const show_result = (startValue, endValue, hValue) =>{
-
         result = []
-        
         while (startValue <= endValue) {
-            var fx1 = fx(startValue).toFixed(5);
+            // var fx1 = fx(startValue).toFixed(5);
             var fx_i = fx_integral(startValue, hValue).toFixed(5);
-            var fx_m = fx_multigral(startValue, hValue).toFixed(5);
+            // var fx_m = fx_multigral(startValue, hValue).toFixed(5);
 
+            // store the result in the dictionary
             record = {};
             record[startValue.toFixed(2)] = fx_i;
             result.push(record); 
@@ -66,14 +65,17 @@ function calc() {
         return result;
     
     }; 
-
+    
+    // print all the records
     result = show_result(startValue, endValue, hValue)
     console.log(result);
-    let it = 0;
 
-    let resultX, resultY;
+    // show the result of min and max backpoints
+    let it = 0;
+    let resultX= 0, resultY=0;
     const valResultMax = document.querySelector('#max');
     const valResultMin = document.querySelector('#min');
+
     while (startValue <= endValue) {
         // console.log('it',it); inrcrease it
 
@@ -95,13 +97,16 @@ function calc() {
             // console.log(result[it][startValue.toFixed(2)])
         } 
     }
+    console.log(resultY, resultX);
 
-    if (resultX != 0 || resultY != 0){
-        valResultMax.innerHTML = `max: {${startValue.toFixed(2)}, ${result[it][startValue.toFixed(2)]}}`;
-        valResultMin.innerHTML = `min: {${Math.abs(startValue.toFixed(2))}, ${result[it][startValue.toFixed(2)]}}`;
-    } else {
+    if (resultX >= 0 && resultY >= 0){
         valResultMax.innerHTML = `max: NOT FOUND`;
         valResultMin.innerHTML = `min: NOT FOUND`;
+    } 
+    
+    else {
+        valResultMax.innerHTML = `max: {${startValue.toFixed(2)}, ${result[it][startValue.toFixed(2)]}}`;
+        valResultMin.innerHTML = `min: {${Math.abs(startValue.toFixed(2))}, ${result[it][startValue.toFixed(2)]}}`;
     }
 
 
